209270bce3a6cdb3c8be69a06f7395552d1e3925
Thiago Trzcinski <thiagotrzcinski7878@gmail.com>
2025-01-17 13:46:37 -0300

    Added roles

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { userRoutes } from './http/controllers/users/routes'
 import { gymRoutes } from './http/controllers/gyms/routes'
 import { checkInsRoutes } from './http/controllers/check-ins/routes'
@@ -9,7 +10,15 @@ export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
 })
+app.register(fastifyCookie)
 
 app.register(userRoutes)
 app.register(gymRoutes)
